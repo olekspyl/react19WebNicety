12a511ee9e8d2ee86eec3971c73ace62d1995189
olekspyl <lemocream@gmail.com>
2025-06-27 09:08:59 +0200

    add fibonacci handler

---
+++ src/BasicState.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+
+export const BasicState = () => {
+    const VAR = [1,1]
+    const [increment, setIncrement] = useState<number>(0)
+    const [FIBONACCI, setFIBONACCI] = useState<number[]>(VAR) 
+
+const fibonacciHandler = (): void => {
+    const copy = [...FIBONACCI]
+    const lastIdx = copy.length-1
+    copy.push(copy[lastIdx] + copy[lastIdx-1])
+    setFIBONACCI(copy)
+}
+
+ type Counter = (action: 'plus' | 'minus') => void
+
+
+    const incrementCounter: Counter = (action) => {
+        if(action === 'plus') {
+        setIncrement(increment+1)
+        } else {
+        setIncrement(increment-1)
+        }
+
+    }
+
+
+
+  return (
+    <div>
+        <button onClick={() =>incrementCounter('plus')}>Click on me to increment</button>
+        <p>increment number {increment}</p>
+        <button onClick={() => incrementCounter('minus')}>Click on me to decrement</button>
+
+        <button onClick={fibonacciHandler}> cLICK to now fibon</button>
+        <div>Fibonacci row : {FIBONACCI.join(',')}</div>
+    </div>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Table from 'table'
+import { BasicState } from 'BasicState'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Table />
+    <BasicState />
   </StrictMode>,
 )
